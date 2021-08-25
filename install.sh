c5205c62cd8d3aaee88c90dcdd4b920cb722394d
Serhii Pererva <serhii.pererva@gmail.com>
2021-08-25 19:44:53 +0100

    No need to install system libraries

--- install.sh
+++ install.sh
@@ -4,16 +4,4 @@ set -eux
 
 # need pip > 19.0 as per https://arrow.apache.org/docs/python/install.html#using-pip
 pip3 install -U pip==21.2.4
-
-# install arrow debian libraries
-sudo apt update
-sudo apt install -y -V ca-certificates lsb-release wget
-wget https://apache.jfrog.io/artifactory/arrow/$(lsb_release --id --short | tr 'A-Z' 'a-z')/apache-arrow-apt-source-latest-$(lsb_release --codename --short).deb
-sudo apt install -y -V ./apache-arrow-apt-source-latest-$(lsb_release --codename --short).deb
-sudo apt update
-sudo apt install -y -V libarrow-dev # For C++
-sudo apt install -y -V libarrow-glib-dev # For GLib (C)
-sudo apt install -y -V libarrow-dataset-dev # For Apache Arrow Dataset C++
-sudo apt install -y -V libarrow-flight-dev # For Apache Arrow Flight C++
-
 pip3 install -r ./requirements.txt
