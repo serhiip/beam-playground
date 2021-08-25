b746f73cef1f160eabaadf3d2a2e17da63e9197f
Serhii Pererva <serhii.pererva@gmail.com>
2021-08-25 19:54:46 +0100

    Change default machine type

--- wordcount_pipeline_streamed.py
+++ wordcount_pipeline_streamed.py
@@ -47,7 +47,7 @@ def run(argv=None, save_main_session=True):
     pipeline_options.view_as(GoogleCloudOptions).job_name = "streaming-wordcount-example"
     pipeline_options.view_as(WorkerOptions).num_workers = 1
     pipeline_options.view_as(WorkerOptions).max_num_workers = 2
-    pipeline_options.view_as(WorkerOptions).machine_type = "f1-micro"
+    pipeline_options.view_as(WorkerOptions).machine_type = "e2-small"
     pipeline_options.view_as(WorkerOptions).disk_size_gb = 1
 
     with beam.Pipeline(options=pipeline_options) as p:
