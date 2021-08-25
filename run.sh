2c879f29ee07c6ce7a85faa68a3bfa95ec1edc7d
Serhii Pererva <serhii.pererva@gmail.com>
2021-08-25 13:00:28 +0100

    Use absolute path

--- run.sh
+++ run.sh
@@ -6,16 +6,17 @@ set -eux
 # gcloud config set project
 
 CURDIR=$(dirname $0)
+CURPATH=$(realpath $CURDIR)
 K8S_ZONES=["\"europe-west6-a"\"]
 REGION=$(gcloud config get-value compute/region)
 PROJECT=$(gcloud config get-value project)
-INPUT_FILENAME=$CURDIR/in.txt
+INPUT_FILENAME=$CURPATH/in.txt
 
-pip3 install -r $CURDIR/requirements.txt
+pip3 install -r $CURPATH/requirements.txt
 
 gcloud services enable dataflow.googleapis.com
 
-pushd $CURDIR/terraform && \
+pushd $CURPATH/terraform && \
     terraform init && \
     terraform apply \
               -var="project_id=$PROJECT" \
