a49734dd7f7fe7d97ee7575fe8f3f3d506cc7429
Serhii Pererva <serhii.pererva@gmail.com>
2021-08-25 12:27:35 +0100

    Add example of running code locally

--- run.sh
+++ run.sh
@@ -30,10 +30,6 @@ rm $INPUT_FILENAME
 
 popd
 
-python test_pipeline.py \
-       --input $BUCKET_URL/$INPUT_FILENAME \
-       --output $BUCKET_URL/counts \
-       --runner DataflowRunner \
-       --project $PROJECT \
-       --region $REGION \
-       --temp_location $BUCKET_URL/tmp/
+python3 ./test_pipeline.py --input $INPUT_FILENAME --output ./out.txt
+
+#python test_pipeline.py --input $BUCKET_URL/kinglear.txt --output $BUCKET_URL/counts --runner DataflowRunner --project $PROJECT --region $REGION --temp_location $BUCKET_URL/tmp/
