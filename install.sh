717d1ce3b00357f56fafb8250a81f9b3be316f51
Serhii Pererva <serhii.pererva@gmail.com>
2021-08-25 12:55:37 +0100

    Install correct version of pip

--- install.sh
+++ install.sh
@@ -2,6 +2,9 @@
 
 set -eux
 
+# need pip > 19.0 as per https://arrow.apache.org/docs/python/install.html#using-pip
+pip3 install -U pip==21.2.4
+
 # install arrow debian libraries
 sudo apt update
 sudo apt install -y -V ca-certificates lsb-release wget
